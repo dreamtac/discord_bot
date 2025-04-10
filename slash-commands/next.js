2edf71f16c374eaf9a4896657a9ac63294cdf4e9
Dongwha <dreamtac@gmail.com>
2025-04-10 22:24:01 +0900

    feat: JWT 토큰 (/링크)

--- slash-commands/next.js
+++ slash-commands/next.js
@@ -1,12 +1,30 @@
 const { ButtonBuilder, ButtonStyle, ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     run: async ({ interaction }) => {
         console.log('next.js 실행');
+        console.log(interaction);
+
+        // JWT 토큰 생성
+        const token = jwt.sign(
+            {
+                userId: interaction.user.id,
+                username: interaction.user.username,
+                nickname: interaction.member?.nickname || interaction.user.username,
+                type: 'vote',
+                exp: Math.floor(Date.now() / 1000) + 60 * 60, // 1시간 후 만료
+            },
+            process.env.JWT_SECRET
+        );
+
+        // 토큰이 포함된 투표 URL 생성
+        const voteUrl = `${process.env.NEXT_URL}/vote?token=${token}`;
+
         const button = new ButtonBuilder({
             label: '투표 페이지로 이동',
             style: ButtonStyle.Link,
-            url: 'http://localhost:3000/vote',
+            url: voteUrl,
         });
         console.log('button 생성');
 
